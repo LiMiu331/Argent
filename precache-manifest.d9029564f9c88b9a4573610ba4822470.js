76b1e6b42c5c3251c5e73d896b73465b2d86b645
limiu <845217637@qq.com>
2020-05-12 16:08:06 +0800

    deploy

---
+++ precache-manifest.d9029564f9c88b9a4573610ba4822470.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1f9cfd6f023abbd8daf",
+    "url": "/Argent/css/app.cc34584e.css"
+  },
+  {
+    "revision": "b4215deb6387ea7fe564531adcff4fe7",
+    "url": "/Argent/index.html"
+  },
+  {
+    "revision": "a1f9cfd6f023abbd8daf",
+    "url": "/Argent/js/app.f1083abd.js"
+  },
+  {
+    "revision": "a615dc4caf6eeb5d6caf",
+    "url": "/Argent/js/chunk-vendors.75e3a155.js"
+  },
+  {
+    "revision": "43f3d4db085ced7e4cb3b5ce92ae5b5b",
+    "url": "/Argent/manifest.json"
+  },
+  {
+    "revision": "fa602ffbdd57cf8da35628b5e0082a21",
+    "url": "/Argent/qr.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Argent/robots.txt"
+  }
+]);
